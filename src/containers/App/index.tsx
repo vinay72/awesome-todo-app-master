18b79a772d85204be8d7d184160bdd1b6274b2f7
vinay72 <vinaysagar@gmail.com>
2021-06-07 03:13:34 -0700

    made personal todo single page application in react redux and typescript

---
+++ src/containers/App/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+import Todos from "../Todos";
+
+const App: React.FC = () => {
+    return (
+        <div>
+            <Todos />
+        </div>
+    );
+};
+
+export default App;
